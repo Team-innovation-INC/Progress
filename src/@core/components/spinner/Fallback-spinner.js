23c4f0e150adceb8915f688365ab10cb73a87050
Raed Rdhaounia <raedrdhaounia@gmail.com>
2022-09-25 14:09:50 +0100

    app : title + log, loader

--- src/@core/components/spinner/Fallback-spinner.js
+++ src/@core/components/spinner/Fallback-spinner.js
@@ -1,15 +1,10 @@
 // ** Logo
-import logo from '@src/assets/images/logo/logo.png'
+import progLogo from '@src/assets/images/spinner/progress.gif'
 
 const SpinnerComponent = () => {
   return (
     <div className='fallback-spinner app-loader'>
-      <img className='fallback-logo' src={logo} alt='logo' />
-      <div className='loading'>
-        <div className='effect-1 effects'></div>
-        <div className='effect-2 effects'></div>
-        <div className='effect-3 effects'></div>
-      </div>
+      <img className='fallback-logo' src={progLogo} alt='logo' />
     </div>
   )
 }
